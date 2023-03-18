c994fbb0d750017b03215a475b0ddd42e15b7fb7
FedericoDavara <davarafederico@gmail.com>
2023-03-17 21:17:48 -0300

    arreglos

--- backend/app.py
+++ backend/app.py
@@ -3,9 +3,9 @@ import os
 
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv
 
 app = Flask(__name__)
 
-if __name__ == "__main__":
-    app.run(debug=True,port=os.getenv("PORT"))
+if __name__ == '__main__':
+    app.run(debug=True,port=os.getenv('PORT'))

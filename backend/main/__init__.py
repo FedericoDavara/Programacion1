a659e5b31e6d93a2789ec9e502ad0024117e676e
FedericoDavara <davarafederico@gmail.com>
2023-03-23 09:49:26 -0300

    arreglos3

--- backend/main/__init__.py
+++ backend/main/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
 import os
-
 from dotenv import load_dotenv
 
-load_dotenv()
-
-app = Flask(__name__)
-
-if __name__ == '__main__':
-    app.run(debug=True,port=os.getenv('PORT'))
+#vamos a crear un metodo que inicializara la app y todos los modulos
+def create_app():
+    #inicio flask
+    app = Flask(__name__)
+    #variables de entorno
+    load_dotenv()
+    return app

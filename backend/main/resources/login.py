460e91489544ace38fc8707a205f5e8310b783f9
ELCOMANDANTE18 <vb.gimenez@alumno.um.edu.ar>
2023-04-12 23:22:37 -0300

    agregando resources

---
+++ backend/main/resources/login.py
@@ -0,0 +1,16 @@
+from flask_restful import Resource
+from flask import request
+
+LOGIN = {
+    1:{"usuario:":"kuzco","contrasena":"nordelta123"}
+}
+
+
+class Login(Resource):
+    def post(self):
+        login = request.get_json()
+        id = int(max(LOGIN.keys()))+1
+        LOGIN[id] = login
+        return LOGIN[id], 201
+    
+    

b40b9375aa1c3d3c567e3470abb2ccb8c8121d5e
ELCOMANDANTE18 <vb.gimenez@alumno.um.edu.ar>
2023-04-13 07:57:05 -0300

    modificacion

--- backend/main/__init__.py
+++ backend/main/__init__.py
@@ -15,5 +15,12 @@ def create_app():
     api.add_resource(resources.UsuarioResource, '/usuario/<id>')
     api.add_resource(resources.UsuariosResource, '/usuarios')
     api.add_resource(resources.UsuarioResource, '/usuario/<id>')
+    api.add_resource(resources.PlanificacionAlumnoResource, "/planificacion_a/<id>")
+    api.add_resource(resources.PlanificacionProfesorResource,"/planificacion_p/<id>")
+    api.add_resource(resources.PlanificacionesProfesoresResource, "/planificaciones_ps")
+    api.add_resource(resources.ProfesorClasesResource,"/clases")
+    api.add_resource(resources.PagoResource, "/pago/<id>")
+    api.add_resource(resources.LoginResource, "/login")
     api.init_app(app)
     return app
+

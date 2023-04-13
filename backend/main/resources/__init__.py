4132229a5b7fa69e42ee4f7011f9e0b9241ba72b
ELCOMANDANTE18 <vb.gimenez@alumno.um.edu.ar>
2023-04-13 18:54:45 -0300

    main

--- backend/main/resources/__init__.py
+++ backend/main/resources/__init__.py
@@ -1,3 +1,8 @@
-from .usuario import Usuario as UsuarioResource
-from .usuario import Usuarios as UsuariosResource
-
+from .usuarios import UsuarioAlumno as UsuarioAlumnoResource
+from .usuarios import Usuarios as UsuariosResource
+from .usuarios import UsuarioProfesor as UsuarioProfesorResource
+from .login import Login as LoginResource
+from .pago import Pago as PagoResource
+from .planificacion import PlanificacionAlumno as PlanificacionAlumnoResource
+from .planificacion import PlanificacionProfesor as PlanificacionProfesorResource
+from .planificacion import PlanificacionesProfesores as PlanificacionesProfesresResources

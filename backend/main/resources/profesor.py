7694edd86c02eae26bc9726588a1bff3bd380b50
FedericoDavara <davarafederico@gmail.com>
2023-04-15 12:05:19 -0300

    tp2fin

--- backend/main/resources/profesor.py
+++ backend/main/resources/profesor.py
@@ -2,7 +2,8 @@ from flask_restful import Resource
 from flask import request
 
 PROFESORES = {
-    1: {"profesor":"Martin Demichellis ","clase":"YOGA"}
+    1: {"profesor":"Martin Demichellis ","clase":"YOGA"},
+    2: {"profesor":"Roman Riquelme ","clase":"CALISTENIA"}
 }
 
 class ProfesorClases(Resource):

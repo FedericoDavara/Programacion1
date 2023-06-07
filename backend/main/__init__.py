d5329da47496e4989fcb0f2b5914da28bf81ad59
ELCOMANDANTE18 <vb.gimenez@alumno.um.edu.ar>
2023-06-07 14:58:40 -0300

    TP7 Ready

--- backend/main/__init__.py
+++ backend/main/__init__.py
@@ -5,13 +5,14 @@ from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
+from flask_mail import Mail
 
 
 api = Api()
 db=SQLAlchemy()
 migrate = Migrate()
 jwt = JWTManager()
-
+mailsender = Mail()
 #metodo que inicializa la app y todos los modulos
 
 def create_app():
@@ -35,23 +36,23 @@ def create_app():
 
     api.add_resource(resources.UsuarioResource, "/usuario/<dni>")
 
-    api.add_resource(resources.UsuariosAlumnosResource, "/usuarios_alumnos")
+    api.add_resource(resources.UsuariosAlumnosResource, "/usuarios_a")
 
-    api.add_resource(resources.UsuarioAlumnoResource, "/usuario_alumno/<dni>")
+    api.add_resource(resources.UsuarioAlumnoResource, "/usuario_a/<dni>")
 
-    api.add_resource(resources.UsuariosProfesoresResource, "/usuarios_profesores")
+    api.add_resource(resources.UsuariosProfesoresResource, "/usuarios_p")
 
-    api.add_resource(resources.UsuarioProfesorResource, "/usuario_profesor/<dni>")
+    api.add_resource(resources.UsuarioProfesorResource, "/usuario_p/<dni>")
 
     api.add_resource(resources.PlanificacionesResource, "/planificaciones")
 
     api.add_resource(resources.PlanificacionResource, "/planificacion/<id>")
 
-    api.add_resource(resources.PlanificacionAlumnoResource, "/planificacion_alumno/<dni>")
+    api.add_resource(resources.PlanificacionAlumnoResource, "/planificacion_a/<dni>")
 
-    api.add_resource(resources.PlanificacionProfesorResource,"/planificacion_profesor/<dni>")
+    api.add_resource(resources.PlanificacionProfesorResource,"/planificacion_p/<dni>")
 
-    api.add_resource(resources.PlanificacionesProfesoresResource, "/planificaciones_profesores")
+    api.add_resource(resources.PlanificacionesProfesoresResource, "/planificaciones_ps")
 
     api.add_resource(resources.ClasesResource,"/clases")
 
@@ -70,6 +71,16 @@ def create_app():
     from main.auth import routes
     app.register_blueprint(routes.auth)
 
+  #Configuración de mail
+    app.config['MAIL_HOSTNAME'] = os.getenv('MAIL_HOSTNAME')
+    app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
+    app.config['MAIL_PORT'] = os.getenv('MAIL_PORT')
+    app.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS')
+    app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+    app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
+    app.config['FLASKY_MAIL_SENDER'] = os.getenv('FLASKY_MAIL_SENDER')
+    #Inicializar en app
+    mailsender.init_app(app)
 
     #retornamos la app inicializada
     return app

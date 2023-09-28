1b4f7a550a919a220306b79fb55f1507ea27ab7d
FedericoDavara <davarafederico@gmail.com>
2023-09-28 04:48:42 -0300

    modificaciones_angular

--- frontend/gol_gym/src/app/app-routing.module.ts
+++ frontend/gol_gym/src/app/app-routing.module.ts
@@ -1,7 +1,40 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { PlanificacionesComponent } from './pages/planificaciones/planificaciones.component';
+import { ClasesComponent } from './pages/clases/clases.component';
+import { ProfesoresComponent } from './pages/profesores/profesores.component';
+import { AltaAlumnoadminComponent } from './pages/alta-alumnoadmin/alta-alumnoadmin.component';
+import { AltaProfesoresadminComponent } from './pages/alta-profesoresadmin/alta-profesoresadmin.component';
+import { BajaProfesoradminComponent } from './pages/baja-profesoradmin/baja-profesoradmin.component';
+import { BajaAlumnoadminComponent } from './pages/baja-alumnoadmin/baja-alumnoadmin.component';
+import { BajaClaseprofeComponent } from './pages/baja-claseprofe/baja-claseprofe.component';
+import { RegistroComponent } from './pages/registro/registro.component';
+import { UsuariosComponent } from './pages/usuarios/usuarios.component';
+import { VistaAdminComponent } from './pages/vista-admin/vista-admin.component';
+import { VistaProfeComponent } from './pages/vista-profe/vista-profe.component';
+import { ErrorPageComponent } from './pages/error-page/error-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home',component: HomeComponent},
+  {path: 'login',component: LoginComponent},
+  {path: 'clases',component: ClasesComponent},
+  {path: 'planificacion',component: PlanificacionesComponent},
+  {path: 'profesores',component: ProfesoresComponent},
+  {path: 'registro',component: RegistroComponent},
+  {path: 'usuarios',component: UsuariosComponent}, 
+  {path: 'vista-admin',component: VistaAdminComponent},
+  {path: 'vista-profe',component: VistaProfeComponent},
+  {path: 'alta-alumnoadmin',component: AltaAlumnoadminComponent},
+  {path: 'alta-profesoresadmin',component: AltaProfesoresadminComponent},
+  {path: 'baja-alumnoadmin',component: BajaAlumnoadminComponent},
+  {path: 'baja-claseprofe',component: BajaClaseprofeComponent},
+  {path: 'baja-profesoradmin',component: BajaProfesoradminComponent},
+  {path: 'error-page',component: ErrorPageComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: '**', redirectTo: 'error-page'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/gol_gym/src/app/app.module.ts
+++ frontend/gol_gym/src/app/app.module.ts
@@ -5,12 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
-import { PlanificacionComponent } from './pages/planificacion/planificacion.component';
 import { ClasesComponent } from './pages/clases/clases.component';
 import { ProfesoresComponent } from './pages/profesores/profesores.component';
 import { AltaAlumnoadminComponent } from './pages/alta-alumnoadmin/alta-alumnoadmin.component';
 import { AltaProfesoresadminComponent } from './pages/alta-profesoresadmin/alta-profesoresadmin.component';
-import { BajaProfesoresadminComponent } from './pages/baja-profesoresadmin/baja-profesoresadmin.component';
 import { BajaProfesoradminComponent } from './pages/baja-profesoradmin/baja-profesoradmin.component';
 import { BajaAlumnoadminComponent } from './pages/baja-alumnoadmin/baja-alumnoadmin.component';
 import { BajaClaseprofeComponent } from './pages/baja-claseprofe/baja-claseprofe.component';
@@ -18,25 +16,29 @@ import { RegistroComponent } from './pages/registro/registro.component';
 import { UsuariosComponent } from './pages/usuarios/usuarios.component';
 import { VistaAdminComponent } from './pages/vista-admin/vista-admin.component';
 import { VistaProfeComponent } from './pages/vista-profe/vista-profe.component';
+import { ErrorPageComponent } from './pages/error-page/error-page.component';
+import { NavComponent } from './components/nav/nav.component';
+import { PlanificacionesComponent } from './pages/planificaciones/planificaciones.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
-    PlanificacionComponent,
     ClasesComponent,
     ProfesoresComponent,
     AltaAlumnoadminComponent,
     AltaProfesoresadminComponent,
-    BajaProfesoresadminComponent,
     BajaProfesoradminComponent,
     BajaAlumnoadminComponent,
     BajaClaseprofeComponent,
     RegistroComponent,
     UsuariosComponent,
     VistaAdminComponent,
-    VistaProfeComponent
+    VistaProfeComponent,
+    ErrorPageComponent,
+    NavComponent,
+    PlanificacionesComponent
   ],
   imports: [
     BrowserModule,

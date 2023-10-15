c336a3dcb77935b327909cb01068dd84a64b4bb5
ELCOMANDANTE18 <vb.gimenez@alumno.um.edu.ar>
2023-10-15 20:58:08 -0300

    tp10.2

--- frontend/gol_gym/src/app/pages/login/login.component.ts
+++ frontend/gol_gym/src/app/pages/login/login.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { AuthService } from 'src/app/services/auth.service'
+import { AuthService } from 'src/app/services/auth.service';
 import { Router } from '@angular/router';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -9,6 +10,7 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 })
 export class LoginComponent {
   loginForm!: FormGroup;
+  isToken: boolean = false; // Inicialmente, el usuario no ha iniciado sesión
 
   constructor(
     private authService: AuthService,
@@ -20,34 +22,40 @@ export class LoginComponent {
     this.loginForm = this.formBuilder.group({
       email: ['cr7@gmail.com', Validators.required],
       password: ['madrid15', Validators.required]
-    })
+    });
   }
 
-  login(dataLogin: any={}){
-    //dataLogin = {email:'cr7@gmail.com', password:'madrid15'}
+  login(dataLogin: any = {}) {
     console.log('comprobando credenciales');
     this.authService.login(dataLogin).subscribe({
       next: (rta: any) => {
         alert('login exitoso');
         console.log('respuesta login:', rta.access_token);
         localStorage.setItem('token', rta.access_token);
+        this.isToken = true; // El usuario ha iniciado sesión, establece isToken en true
         this.router.navigateByUrl('home');
-      },error: (error) => {
+      },
+      error: (error) => {
         alert('Credenciales incorrectas');
         localStorage.removeItem('token');
-
-      }, complete: () => {
-        console.log('finalizo')
+      },
+      complete: () => {
+        console.log('finalizo');
       }
-    })
+    });
   }
   
-  submit(){
-    if(this.loginForm.valid){
-      console.log('Form login: ',this.loginForm.value);
-      this.login (this.loginForm.value);
+  submit() {
+    if (this.loginForm.valid) {
+      console.log('Form login: ', this.loginForm.value);
+      this.login(this.loginForm.value);
     } else {
-      alert('Formulario invalido');
+      alert('Formulario inválido');
     }
+  }
+  
+  cerrarSesion() {
+    localStorage.removeItem('token');
+    this.isToken = false; // El usuario ha cerrado sesión, establece isToken en false
   }
 }

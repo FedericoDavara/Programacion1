48d4bf21a0ef6d2bf9fe28c0ccd2bea0fe723571
ELCOMANDANTE18 <vb.gimenez@alumno.um.edu.ar>
2023-11-16 01:12:05 -0300

    Co-authored-by: Adriano Gabriel Tisera Aguilera <AdrianoTisera@users.noreply.github.com>

--- frontend/gol_gym/src/app/pages/login/login.component.ts
+++ frontend/gol_gym/src/app/pages/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 import { Router } from '@angular/router';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { jwtDecode } from "/home/victor/Escritorio/Nueva carpeta 1 (copia)/Programacion1/frontend/gol_gym/node_modules/jwt-decode/build/cjs/index"
+
 
 @Component({
   selector: 'app-login',
@@ -25,37 +27,44 @@ export class LoginComponent {
     });
   }
 
-  login(dataLogin: any = {}) {
+  login(dataLogin:any = {} ){
+    //dataLogin = {email: 'johnnylawrence@nomail.com', password: 'clave1234'}
     console.log('comprobando credenciales');
     this.authService.login(dataLogin).subscribe({
-      next: (rta: any) => {
-        alert('login exitoso');
-        console.log('respuesta login:', rta.access_token);
-        localStorage.setItem('token', rta.access_token);
-        this.isToken = true; // El usuario ha iniciado sesión, establece isToken en true
-        this.router.navigateByUrl('home');
-      },
-      error: (error) => {
-        alert('Credenciales incorrectas');
-        localStorage.removeItem('token');
-      },
-      complete: () => {
-        console.log('finalizo');
+      next: (rta:any) => {
+        console.log('Respuesta login: ',rta.access_token);
+        localStorage.setItem('token', rta.access_token)
+
+        const decodedToken: any = jwtDecode(rta.access_token);
+        localStorage.setItem('role', decodedToken.rol)
+        localStorage.setItem('dni', decodedToken.dni)
+
+        if (localStorage.getItem('role') === 'admin' || localStorage.getItem('role') === 'profesor') {
+          this.router.navigateByUrl('usuarios');
+        } else if (localStorage.getItem('role') === 'user') {
+          this.router.navigateByUrl('/vPerfil');
+        } else {
+          console.error('No posee rol de usuario');
+        }
+
+      }, error:(error) => {
+          alert('Credenciales incorrectas');
+          localStorage.removeItem('dni');
+          localStorage.removeItem('role');
+          localStorage.removeItem('token');
+          
+      }, complete: () => {
+        console.log('Finalizo')
       }
-    });
+    })
   }
 
   submit() {
-    if (this.loginForm.valid) {
-      console.log('Form login: ', this.loginForm.value);
-      this.login(this.loginForm.value);
+    if(this.loginForm.valid) {
+      console.log('Form login: ',this.loginForm.value);
+      this.login(this.loginForm.value)
     } else {
       alert('Formulario inválido');
     }
-  }
-
-  cerrarSesion() {
-    localStorage.removeItem('token');
-    this.isToken = false; // El usuario ha cerrado sesión, establece isToken en false
   }
 }

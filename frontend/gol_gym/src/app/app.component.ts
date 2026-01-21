70b7003c075e652fa40e3212db7af3eaceb9813d
AdrianoTisera <adrianotisera@gmail.com>
2026-01-21 18:19:30 -0300

    alerta

--- frontend/gol_gym/src/app/app.component.ts
+++ frontend/gol_gym/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent{
+export class AppComponent implements OnInit {
   title = 'app_gym';
+  isAlertVisible = true;
+  isAlertClosing = false;
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.hideAlert();
+    }, 5000);
+  }
+
+  hideAlert(): void {
+    this.isAlertClosing = true;
+    setTimeout(() => {
+      this.isAlertVisible = false;
+    }, 500);
+  }
 }
